fef870d5b4cd4d26ffba9983292a2afd5c3029e8
Erik Olofsson <erik@olofsson.info>
2019-05-16 08:50:23 +0200

    [Malterlib/Meteor] Use brew instead of MacPorts

--- Apps/MeteorManager/Source/Malterlib_Meteor_App_MeteorManagerDaemon.cpp
+++ Apps/MeteorManager/Source/Malterlib_Meteor_App_MeteorManagerDaemon.cpp
@@ -12,8 +12,8 @@ namespace NMib::NMeteor::NMeteorManager
 	{
 #ifdef DPlatformFamily_OSX
 		CStr Path = fg_GetSys()->f_GetEnvironmentVariable("PATH");
-		if (Path.f_Find("/opt/local/bin") < 0)
-			fg_GetSys()->f_SetEnvironmentVariable("PATH", "/opt/local/bin:" + Path);
+		if (Path.f_Find("/usr/local/bin") < 0)
+			fg_GetSys()->f_SetEnvironmentVariable("PATH", "/usr/local/bin:" + Path);
 #endif
 		if (mp_Options.m_bUseInternalNode)
 		{
@@ -22,17 +22,17 @@ namespace NMib::NMeteor::NMeteorManager
 			fg_GetSys()->f_SetEnvironmentVariable("PATH", NodeBinDirectory + ":" + Path);
 		}
 	}
-	
+
 	CMeteorManagerDaemonActor::~CMeteorManagerDaemonActor()
 	{
 	}
-	
+
 	void CMeteorManagerDaemonActor::fp_PopulateAppInterfaceRegisterInfo(CDistributedAppInterfaceServer::CRegisterInfo &o_RegisterInfo, NEncoding::CEJSON const &_Params)
 	{
 		o_RegisterInfo.m_UpdateType = EDistributedAppUpdateType_AllAtOnce;
 
 		mint nNodes = 0;
-		
+
 		for (auto &Package : mp_Options.m_Packages)
 			nNodes += Package.m_Concurrency;
 
@@ -43,32 +43,32 @@ namespace NMib::NMeteor::NMeteorManager
 		mint nFilesPerProc = 0;
 		nFilesPerProc = fg_Max(nFilesPerProc, CMeteorManagerActor::fs_GetNginxWorkerFileLimits());
 		nFilesPerProc = fg_Max(nFilesPerProc, CMeteorManagerActor::fs_GetNodeFileLimits());
-		
+
 		mint nMaxThreads = 1024;
 
 		mint nMaxPids = 32 + nNodes;
-		
+
 		o_RegisterInfo.m_Resources_Files = nMaxFilesNeeded;
-		o_RegisterInfo.m_Resources_Threads = nMaxThreads; 
+		o_RegisterInfo.m_Resources_Threads = nMaxThreads;
 		o_RegisterInfo.m_Resources_FilesPerProcess = nFilesPerProc;
-		o_RegisterInfo.m_Resources_Processes = nMaxPids; 
+		o_RegisterInfo.m_Resources_Processes = nMaxPids;
 	}
-	
+
 	TCFuture<void> CMeteorManagerDaemonActor::fp_StartApp(NEncoding::CEJSON const &_Params)
 	{
 		mp_pManager = fg_ConstructActor<CMeteorManagerActor>(fg_Construct(self), mp_State, mp_Options);
-		
+
 		return mp_pManager(&CMeteorManagerActor::f_Startup);
 	}
-	
+
 	TCFuture<void> CMeteorManagerDaemonActor::fp_StopApp()
-	{	
+	{
 		TCSharedPointer<CCanDestroyTracker> pCanDestroy = fg_Construct();
-		
+
 		if (mp_pManager)
 		{
 			DMibLogWithCategory(Daemon, Info, "Shutting down");
-			
+
 			mp_pManager->f_Destroy() > [pCanDestroy](TCAsyncResult<void> &&_Result)
 				{
 					if (!_Result)
@@ -77,17 +77,17 @@ namespace NMib::NMeteor::NMeteorManager
 			;
 			mp_pManager = nullptr;
 		}
-		
+
 		return pCanDestroy->f_Future();
 	}
-	
+
 	TCFuture<void> CMeteorManagerDaemonActor::fp_PreStop()
 	{
 		if (!mp_pManager)
 			return fg_Explicit();
 
 		DMibLogWithCategory(Daemon, Info, "Running pre-stop");
-		
+
 		TCPromise<void> Promise;
 		mp_pManager(&CMeteorManagerActor::f_PreStop) > [Promise](TCAsyncResult<void> &&_Result)
 			{
