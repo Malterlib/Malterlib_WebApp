5cf45f89bad8b721b76b2d44209603f33aa7071e
Erik Olofsson <erik@olofsson.info>
2019-10-13 20:23:43 +0200

    [Malterlib/Meteor] Fix incorrect usage of f_Split*

--- Apps/MeteorManager/Source/Malterlib_Meteor_App_MeteorManager_Node.cpp
+++ Apps/MeteorManager/Source/Malterlib_Meteor_App_MeteorManager_Node.cpp
@@ -13,7 +13,7 @@ namespace NMib::NMeteor::NMeteorManager
 	{
 		return 65536 + 8192;
 	}
-	
+
 	CStr CMeteorManagerActor::fp_GetNodeExecutable(CStr const &_Executable)
 	{
 #ifdef DPlatformFamily_Windows
@@ -28,7 +28,7 @@ namespace NMib::NMeteor::NMeteorManager
 			return "node";
 #endif
 	}
-	
+
 	CStr CMeteorManagerActor::fp_GetPackageHostname(CStr const &_PackageName, EHostnamePrefix _Prefix) const
 	{
 		auto &Package = mp_Options.m_Packages[_PackageName];
@@ -49,7 +49,7 @@ namespace NMib::NMeteor::NMeteorManager
 
 		return fg_Format("{}.{}", Prefix, mp_Domain);
 	}
-	
+
 	CStr CMeteorManagerActor::fp_GetRootURL(CStr const &_Hostname, CStr const &_SubPath) const
 	{
 		if (!_SubPath.f_IsEmpty())
@@ -82,12 +82,12 @@ namespace NMib::NMeteor::NMeteorManager
 	{
 		auto &CurrentURL = mp_CurrentPackageLocalURL[_PackageName];
 		mint iURL = CurrentURL;
-		
+
 		auto &Package = mp_Options.m_Packages[_PackageName];
 		++CurrentURL;
 		if (CurrentURL >= Package.m_Concurrency)
 			CurrentURL = 0;
-		
+
 		return mp_PackageLocalURLs[_PackageName][iURL];
 	}
 
@@ -116,9 +116,9 @@ namespace NMib::NMeteor::NMeteorManager
 					AppLaunch.m_LogCategory = fg_Format("{}-{}", LaunchKey.m_PackageName, i);
 				else
 					AppLaunch.m_LogCategory = LaunchKey.m_PackageName;
-				
+
 				AppLaunch.m_BackendIdentifier = fg_Format("{}_{}", LaunchKey.m_PackageName, fg_RandomID());
-				
+
 				mp_PackageLocalURLs[LaunchKey.m_PackageName].f_Insert(fp_GetAppLocalURL(AppLaunch));
 			}
 		}
@@ -143,24 +143,24 @@ namespace NMib::NMeteor::NMeteorManager
 
 		return Promise <<= mp_AppLaunchesPromise.f_Future();
 	}
-	
+
 	TCFuture<void> CMeteorManagerActor::fp_DestroyApps()
 	{
 		TCPromise<void> Promise;
 
 		TCActorResultVector<void> Destroys;
-		
+
 		for (auto &Launch : mp_AppLaunches)
 		{
 			if (!Launch.m_Launch)
 				continue;
 			Launch.m_Launch.f_Destroy() > Destroys.f_AddResult();
 		}
-	
+
 		Destroys.f_GetResults() > Promise.f_ReceiveAny();
 		return Promise.f_MoveFuture();
 	}
-	
+
 	void CMeteorManagerActor::fp_UpdateAppLaunch(CExceptionPointer const &_pException)
 	{
 		if (_pException)
@@ -169,7 +169,7 @@ namespace NMib::NMeteor::NMeteorManager
 				mp_AppLaunchesPromise.f_SetException(_pException);
 			return;
 		}
-		
+
 		for (auto &Launch : mp_AppLaunches)
 		{
 			auto &LaunchKey = Launch.f_GetKey();
@@ -200,10 +200,10 @@ namespace NMib::NMeteor::NMeteorManager
 			if (Launch.m_bInitialLaunched)
 				continue;
 			Launch.m_bInitialLaunched = true;
-			
+
 			fp_LaunchApp(Launch, true);
 		}
-		
+
 		bool bAllDone = true;
 		for (auto &Outstanding : mp_OutstandingLaunches)
 		{
@@ -217,11 +217,11 @@ namespace NMib::NMeteor::NMeteorManager
 		if (bAllDone && !mp_AppLaunchesPromise.f_IsSet())
 			mp_AppLaunchesPromise.f_SetResult();
 	}
-	
+
 	void CMeteorManagerActor::fp_SetupNodeArguments(TCVector<CStr> &o_Arguments, CAppLaunch const &_AppLaunch, CMeteorManagerOptions::CPackage const &_PackageOptions)
 	{
 		o_Arguments.f_Insert(fg_Format("--max_old_space_size={}", (_PackageOptions.m_MemoryPerNode*1024.0).f_ToInt()));
-		
+
 		if (fp_GetConfigValue("NodeDebug", false).f_Boolean())
 			o_Arguments.f_Insert("--inspect={}:5599"_f << fp_GetAppIPAddress(_AppLaunch));
 		else
@@ -241,7 +241,7 @@ namespace NMib::NMeteor::NMeteorManager
 		TCSet<CStr> AvailableTags = mp_Tags;
 		AvailableTags[_PackageOptions.f_GetName()];
 		AvailableTags[fg_Format("{}_{}", _PackageOptions.f_GetName(), _AppLaunch.f_GetKey().m_iAppSequence)];
-		
+
 		switch(_PackageOptions.m_Type)
 		{
 		case CMeteorManagerOptions::EPackageType_Meteor: AvailableTags["Meteor"]; break;
@@ -251,7 +251,7 @@ namespace NMib::NMeteor::NMeteorManager
 		case CMeteorManagerOptions::EPackageType_Custom: AvailableTags["Custom"]; break;
 		case CMeteorManagerOptions::EPackageType_Static: AvailableTags["Static"]; break;
 		}
-		
+
 		for (auto &EnvVar : mp_Options.m_Environment)
 		{
 			bool bPassAllTags = true;
@@ -276,29 +276,29 @@ namespace NMib::NMeteor::NMeteorManager
 
 			if (!bPassAllTags)
 				continue;
-			
+
 			if (auto *pSetting = _CalculatedSettings.f_FindEqual(EnvVar.m_Setting))
 			{
 				o_Environment[EnvVar.f_GetName()] = *pSetting;
 				continue;
 			}
-			
+
 			CEJSON Value;
 			if (EnvVar.m_Default)
 				Value = fp_GetConfigValue(EnvVar.m_Setting, *EnvVar.m_Default);
 			else
 				Value = fp_GetConfigValue(EnvVar.m_Setting, nullptr);
-			
+
 			if (Value.f_IsNull())
 				continue;
-			
+
 			o_Environment[EnvVar.f_GetName()] = Value.f_AsString();
 		}
 	}
 
 	void CMeteorManagerActor::fp_HandleNodeDebuggerOutput(CAppLaunch &_AppLaunch, CStr const &_StdErr)
 	{
-		for (auto &Line : _StdErr.f_SplitLine())
+		for (auto &Line : _StdErr.f_SplitLine<true>())
 		{
 			if (!Line.f_StartsWith("Debugger listening on "))
 				continue;
@@ -346,20 +346,20 @@ namespace NMib::NMeteor::NMeteorManager
 	{
 		if (_AppLaunch.m_Launch || mp_bStopped || f_IsDestroyed())
 			return; // Launch already in progress
-		
+
 		CStr ProgramDirectory = CFile::fs_GetProgramDirectory();
-		
+
 		TCVector<CStr> Arguments;
 		auto *pAppLaunch = &_AppLaunch;
 		auto &LaunchKey = _AppLaunch.f_GetKey();
-		
+
 		CStr LaunchExecutable;
 		CStr PackageDirectory = fg_Format("{}/{}", ProgramDirectory, _AppLaunch.f_GetKey().m_PackageName);
 
 		auto &PackageOptions = fg_Const(mp_Options.m_Packages)[LaunchKey.m_PackageName];
 
 		bool bIsNode = false;
-		
+
 		if (PackageOptions.m_Type == CMeteorManagerOptions::EPackageType_Meteor)
 		{
 #ifdef DPlatformFamily_Windows
@@ -369,7 +369,7 @@ namespace NMib::NMeteor::NMeteorManager
 #endif
 			fp_SetupNodeArguments(Arguments, _AppLaunch, PackageOptions);
 			bIsNode = true;
-			
+
 			Arguments.f_Insert(fg_Format("{}/{}/main.js", ProgramDirectory, LaunchKey.m_PackageName));
 		}
 		else if (PackageOptions.m_Type == CMeteorManagerOptions::EPackageType_Npm)
@@ -381,7 +381,7 @@ namespace NMib::NMeteor::NMeteorManager
 #endif
 			fp_SetupNodeArguments(Arguments, _AppLaunch, PackageOptions);
 			bIsNode = true;
-			
+
 			Arguments.f_Insert(ProgramDirectory / LaunchKey.m_PackageName / PackageOptions.m_MainFile);
 		}
 		else if (PackageOptions.m_Type == CMeteorManagerOptions::EPackageType_Custom)
@@ -423,7 +423,7 @@ namespace NMib::NMeteor::NMeteorManager
 				fp_UpdateAppLaunch(fg_ExceptionPointer(DMibErrorInstance("Invalid package type")));
 			return;
 		}
-		
+
 		CProcessLaunchActor::CLaunch Launch = CProcessLaunchParams::fs_LaunchExecutable
 			(
 				LaunchExecutable
@@ -432,9 +432,9 @@ namespace NMib::NMeteor::NMeteorManager
 				, [this, pAppLaunch, _bInitialLaunch](CProcessLaunchStateChangeVariant const &_Change, fp64 _TimeSinceStart)
 				{
 					auto &AppLaunch = *pAppLaunch;
-					
+
 					DLogCategoryStr(AppLaunch.m_LogCategory);
-					
+
 					switch (_Change.f_GetTypeID())
 					{
 					case EProcessLaunchState_Launched:
@@ -462,7 +462,7 @@ namespace NMib::NMeteor::NMeteorManager
 							{
 								if (_bInitialLaunch)
 									fp_UpdateAppLaunch(fg_ExceptionPointer(DMibErrorInstance(fg_Format("Unexpected exit {}", _Change.f_Get<EProcessLaunchState_Exited>()))));
-								
+
 								DLog(Info, "Unexpected exit {}, scheduling relaunch in 10 seconds", _Change.f_Get<EProcessLaunchState_Exited>());
 								fg_Timeout(10.0) > [this, pAppLaunch]
 									{
@@ -503,7 +503,7 @@ namespace NMib::NMeteor::NMeteorManager
 		Launch.m_LogName = _AppLaunch.m_LogCategory;
 		Launch.m_Params.m_bCreateNewProcessGroup = true;
 		Launch.m_bWholeLineOutput = true;
-		
+
 		auto &Params = Launch.m_Params;
 
 		Params.m_bAllowExecutableLocate = true;
@@ -561,7 +561,7 @@ namespace NMib::NMeteor::NMeteorManager
 		Params.m_Environment["TMP"] = LaunchHomePath + "/.tmp";
 		Params.m_Environment["TEMP"] = LaunchHomePath + "/.tmp";
 #endif
-		
+
 		try
 		{
 			if (auto *pValue = mp_AppState.m_ConfigDatabase.m_Data.f_GetMember("NodeEnvironment"))
@@ -573,20 +573,20 @@ namespace NMib::NMeteor::NMeteorManager
 					Params.m_Environment[EnvVar.f_Name()] = EnvVar.f_Value().f_String();
 				}
 			}
-			
+
 			TCMap<CStr, CStr> CalculatedSettings;
 			CalculatedSettings["Home"] = LaunchHomePath;
 			CalculatedSettings["TmpDir"] = LaunchHomePath + "/.tmp";
-			
+
 			if (mp_bIsStaging)
 				CalculatedSettings["Staging"] = "true";
 
 			CalculatedSettings["PackageDirectory"] = PackageDirectory;
-			
+
 			CalculatedSettings["ManagerInstanceID"] = mp_InstanceId;
 			CalculatedSettings["LaunchInstanceID"] = fg_RandomID();
 			CalculatedSettings["LaunchSequence"] = CStr::fs_ToStr(_AppLaunch.f_GetKey().m_iAppSequence);
-			
+
 			CalculatedSettings["PackageName"] = CStr::fs_ToStr(_AppLaunch.f_GetKey().m_PackageName);
 			CalculatedSettings["BackendIdentifier"] = _AppLaunch.m_BackendIdentifier;
 			CalculatedSettings["LocalIP"] = fp_GetAppIPAddress(_AppLaunch);
@@ -616,13 +616,13 @@ namespace NMib::NMeteor::NMeteorManager
 				CalculatedSettings[fg_Format("RootURL_{}", Package.f_GetName())] = fp_GetRootURL(Hostname, PackageOptions.m_SubPath);
 				CalculatedSettings[fg_Format("LocalURL_{}", Package.f_GetName())] = fp_GetPackageLocalURL(Package.f_GetName());
 			}
-			
+
 			CStr MongoSSLDirectory = fp_GetMongoSSLDirectory();
 			if (!MongoSSLDirectory.f_IsEmpty())
 			{
 				CalculatedSettings["IsMultiHost"] = "true";
 				CalculatedSettings["DDPSelf"] = mp_DDPSelf;
-				
+
 				CStr CaCertificatePath = LaunchHomePath + "/certificates/MongoCA.crt";
 				CStr ClientCertificatePath = LaunchHomePath + "/certificates/admin.pem";
 				CStr UserNameEncoded;
@@ -640,7 +640,7 @@ namespace NMib::NMeteor::NMeteorManager
 						, mp_MongoReplicaName
 					)
 				;
-				
+
 				CalculatedSettings["MongoOplogURL"] = fg_Format
 					(
 						"mongodb://{}@{}:{}/local?authMechanism=MONGODB-X509"
@@ -660,7 +660,7 @@ namespace NMib::NMeteor::NMeteorManager
 				CalculatedSettings["MongoURL"] = fg_Format("mongodb://localhost:{}/{}", mp_MongoPort, mp_MongoDatabase);
 				CalculatedSettings["MongoOplogURL"] = fg_Format("mongodb://localhost:{}/local", mp_MongoPort);
 			}
-			
+
 			CJSON MeteorSettings;
 			{
 				auto &PublicMeteorSettings = MeteorSettings["public"];
@@ -687,7 +687,7 @@ namespace NMib::NMeteor::NMeteorManager
 				if (mp_bIsStaging)
 					PublicMeteorSettings["stagingServer"] = true;
 			}
-			
+
 			if (mp_pCustomization)
 			{
 				mp_pCustomization->f_CalculateSettings
@@ -705,7 +705,7 @@ namespace NMib::NMeteor::NMeteorManager
 					)
 				;
 			}
-			
+
 			fp_PopulateNodeEnvironment(Params.m_Environment, CalculatedSettings, _AppLaunch, PackageOptions);
 
 			if (PackageOptions.m_Type == CMeteorManagerOptions::EPackageType_Meteor)
@@ -719,14 +719,14 @@ namespace NMib::NMeteor::NMeteorManager
 				fp_UpdateAppLaunch(fg_ExceptionPointer(DMibErrorInstance(fg_Format("Failed to setup node launch environment: {}", _Exception))));
 			return;
 		}
-	
+
 		_AppLaunch.m_Launch = fg_ConstructActor<CProcessLaunchActor>();
-		
+
 		_AppLaunch.m_Launch(&CProcessLaunchActor::f_Launch, fg_Move(Launch), fg_ThisActor(this))
 			> [this, pAppLaunch, _bInitialLaunch](TCAsyncResult<CActorSubscription> &&_Subscription)
 			{
 				auto &AppLaunch = *pAppLaunch;
-				
+
 				if (!_Subscription)
 				{
 					if (_bInitialLaunch)
