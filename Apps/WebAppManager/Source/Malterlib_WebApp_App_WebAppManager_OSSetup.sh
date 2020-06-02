2103a3d6e1629deaf81893c379bf3de0ce295d32
Erik Olofsson <erik.olofsson@favro.com>
2020-06-02 19:43:24 +0200

    [Malterlib/WebApp/WebAppManager] Allow loopback prefix to be 0

--- Apps/WebAppManager/Source/Malterlib_WebApp_App_WebAppManager_OSSetup.sh
+++ Apps/WebAppManager/Source/Malterlib_WebApp_App_WebAppManager_OSSetup.sh
@@ -6,19 +6,19 @@ if [ "$PlatformFamily" == "OSX" ]; then
 
 	COUNTER=0
 	while [ $COUNTER -lt $NumNodeServers ]; do
-		let IPAddress=COUNTER+2
+		let IPAddress=COUNTER+2 1
 		ifconfig lo0 alias "127.$LoopbackPrefix.$IPAddress.1/32"
-		let COUNTER=COUNTER+1 
+		let COUNTER=COUNTER+1 1
 	done
 
 elif  [ "$PlatformFamily" == "Linux" ]; then
 
 	COUNTER=0
 	while [ $COUNTER -lt $NumNodeServers ]; do
-		let IPAddress=COUNTER+2
-		let InterfaceAddress=$LoopbackPrefix*256+$COUNTER
+		let IPAddress=COUNTER+2 1
+		let InterfaceAddress=$LoopbackPrefix*256+$COUNTER 1
 		ifconfig lo:$InterfaceAddress 127.$LoopbackPrefix.$IPAddress.1 netmask 255.255.255.255
-		let COUNTER=COUNTER+1 
+		let COUNTER=COUNTER+1 1
 	done
 
 elif  [ "$PlatformFamily" == "Windows" ]; then
