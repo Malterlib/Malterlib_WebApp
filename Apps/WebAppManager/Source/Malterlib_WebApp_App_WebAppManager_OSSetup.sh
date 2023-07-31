a0bf612cced24bbda2cb43bbc020c9d2b3f3db0b
Erik Olofsson <erik@olofsson.info>
2023-07-31 22:08:52 +0200

    [Malterlib/WebApp/WebAppManager] Use ip command instead of ifconfig on Linux

--- Apps/WebAppManager/Source/Malterlib_WebApp_App_WebAppManager_OSSetup.sh
+++ Apps/WebAppManager/Source/Malterlib_WebApp_App_WebAppManager_OSSetup.sh
@@ -16,8 +16,12 @@ elif  [ "$PlatformFamily" == "Linux" ]; then
 	COUNTER=0
 	while [ $COUNTER -lt $NumNodeServers ]; do
 		let IPAddress=COUNTER+2 1
-		let InterfaceAddress=$LoopbackPrefix*256+$COUNTER 1
-		ifconfig lo:$InterfaceAddress 127.$LoopbackPrefix.$IPAddress.1 netmask 255.255.255.255
+		if which ip; then
+			ip -4 addr replace 127.$LoopbackPrefix.$IPAddress.1/32 dev lo
+		else
+			let InterfaceAddress=$LoopbackPrefix*256+$COUNTER 1
+			ifconfig lo:$InterfaceAddress 127.$LoopbackPrefix.$IPAddress.1 netmask 255.255.255.255
+		fi
 		let COUNTER=COUNTER+1 1
 	done
 
